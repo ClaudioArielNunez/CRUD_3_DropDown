bcf978c611d4ee27727232528ff0905ec4b0aaaa
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-05-18 17:08:39 -0300

    Detalles de vistas

--- CRUD_2_DropDown/Models/Producto.cs
+++ CRUD_2_DropDown/Models/Producto.cs
@@ -10,15 +10,15 @@ namespace CRUD_2_DropDown.Models
         public string Nombre { get; set; }
         public double Precio { get; set; }
         public int Cantidad { get; set; }
-        [ForeignKey("Categorias")]  //Definimos FK, debe coincidir con la prop de navegacion si hay, sino lleva el nombre de la FK
+        [ForeignKey("Categorias")]  //Definimos FK, debe coincidir con la prop de navegacion si hay, sino lleva el nombre de la FK,al tener el mismo nombre,garantizas que Entity Framework entiende cómo las entidades están relacionadas 
         public int CategoriaId { get; set; }
         public virtual Categoria Categorias { get; set; }
-        //Propiedad de navegación que permite acceder a la entidad Categoria asociada,
+        //Propiedad de Navegación (Categorias): Proporciona acceso a la entidad relacionada        
         //facilita la carga de la entidad Categoria relacionada, permiten navegar a través de las relaciones entre entidades en el código.
         //permite acceder a los detalles de la categoría desde una instancia de Producto.
         //Usar virtual en propiedades de navegación es una práctica común y recomendada cuando se trabaja con Entity Framework
         //y quieres aprovechar la carga diferida, esta permite que las entidades relacionadas se carguen
         //automáticamente cuando se acceden por primera vez, mejorando el rendimiento en algunos escenarios.
     }
-    
+
 }
