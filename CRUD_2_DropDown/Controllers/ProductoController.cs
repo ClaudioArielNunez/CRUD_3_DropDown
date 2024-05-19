67a4108ae76f7bd3dc0ddb19de39aa4af67c785e
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-05-18 21:11:45 -0300

    Vista crear, dropDown

--- CRUD_2_DropDown/Controllers/ProductoController.cs
+++ CRUD_2_DropDown/Controllers/ProductoController.cs
@@ -1,6 +1,11 @@
 ﻿using CRUD_2_DropDown.DataLayer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Differencing;
 using Microsoft.EntityFrameworkCore;
+using static System.Net.WebRequestMethods;
+using System;
+using CRUD_2_DropDown.Models;
 
 namespace CRUD_2_DropDown.Controllers
 {
@@ -14,8 +19,33 @@ namespace CRUD_2_DropDown.Controllers
         }
         public IActionResult Index()
         {
-            var productos = context.Productos.Include("Categorias");
+            //Al usar Include, la consulta carga los Productos y sus Categorias en una sola operación,mejorando la eficiencia
+            var productos = context.Productos.Include("Categorias").ToList();
             return View(productos);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            //Cargamos categorias
+            CargarCategorias();
+            return View();
+        }
+
+        //[NonAction]: Evita que el método sea llamado como una acción HTTP.
+        [NonAction]
+        private void CargarCategorias()
+        {
+            var categorias = context.Categorias.ToList();
+            ViewBag.Categorias = new SelectList(categorias, "Id", "Nombre");
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Producto producto)
+        {            
+            context.Productos.Add(producto);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
